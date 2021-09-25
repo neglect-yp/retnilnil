70d8b91a587c8f5df375e6dfb8ea412d53023f39
neglect-yp <grandis.hebitombo@gmail.com>
2021-09-25 20:03:32 +0900

    test: interface

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -6,18 +6,32 @@ type T struct {
 	I int
 }
 
+type IF interface{}
+
 func f() (*T, error) {
 	return &T{I: 1}, nil
 }
 
 func f2() (*T, error) {
-	return nil, nil // want "return nil, nil"
+	return nil, errors.New("error")
 }
 
 func f3() (*T, error) {
-	return nil, errors.New("error")
+	return nil, nil // want "return nil, nil"
 }
 
 func f4() (T, error) {
 	return T{}, nil
+}
+
+func f5() (IF, error) {
+	return &T{}, nil
+}
+
+func f6() (IF, error) {
+	return &T{}, errors.New("error")
+}
+
+func f7() (IF, error) {
+	return nil, nil // want "return nil, nil"
 }
